e67c6801209e5889d26b5710a4c0d982d1c2d3c9
rryuzakiii <itsamitkumar5393@gmail.com>
2024-08-23 00:35:51 +0530

    strings

---
+++ test2.js
@@ -0,0 +1,15 @@
+const greeting = "Hello";
+const name = "chris";
+console.log(greeting + ", " + name);
+
+const thora = "bkl";
+const lol = "Apple";
+console.log(`${thora}, ${lol}`);
+
+const song = "Fight the Youth";
+const score = 9;
+const highestScore = 10;
+const output = `I like the song ${song}. I gave it a score of ${
+  (score / highestScore) * 100
+}%.`;
+console.log(output); // "I like the song Fight the Youth. I gave it a score of 90%."
